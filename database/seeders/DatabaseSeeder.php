22dd12e8d59abf311f5ef620753f4d8710149a3e
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-02-05 02:44:38 -0300

    criando seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,68 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('profiles')->insert(
+            [
+                'codigo' => 1,
+                'nome' => 'Administrador',
+                'slug' => 'administrador',
+                'descricao' => 'Administrador do Sistema'
+            ]
+        );
+
+        DB::table('profiles')->insert(
+
+            [
+                'codigo' => 2,
+                'nome' => 'Gerente de Projetos',
+                'slug' => 'gerente_de_projetos',
+                'descricao' => 'Gerente de Projetos do Sistema'
+            ]
+        );
+
+        DB::table('profiles')->insert(
+
+            [
+                'codigo' => 3,
+                'nome' => 'Membro',
+                'slug' => 'membro',
+                'descricao' => 'Membro do Sistema'
+            ]
+        );
+
+        DB::table('status')->insert(
+            [
+                'codigo' => 1,
+                'nome' => 'Rascunho',
+                'slug' => 'rascunho',
+                'descricao' => 'Rascunho'
+            ]
+        );
+        DB::table('status')->insert(
+
+            [
+                'codigo' => 2,
+                'nome' => 'Em Andamento',
+                'slug' => 'em_andamento',
+                'descricao' => 'Em Andamento'
+            ]
+        );
+        DB::table('status')->insert(
+
+            [
+                'codigo' => 3,
+                'nome' => 'Concluido',
+                'slug' => 'concluido',
+                'descricao' => 'Concluido'
+            ],
+        );
+
         DB::table('users')->insert([
-            'username' => 'guilherme',
-            'firstname' => 'Guilherme',
-            'lastname' => 'Delmiro',
-            'email' => 'guilherme.delmiro@gmail.com',
+            'username' => 'user',
+            'firstname' => 'User',
+            'lastname' => 'Admin',
+            'email' => 'email@email.com',
+            'profile_id' => 1,
             'password' => bcrypt('P@ssword')
         ]);
     }
