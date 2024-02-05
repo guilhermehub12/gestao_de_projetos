5f3a9c763de2f582d789903a825fefcc4f280bd9
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-02-05 02:41:55 -0300

    corrigindo migrations

---
+++ database/migrations/2024_02_04_151401_create_projects_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nome');
+            $table->string('localizacao');
+            $table->string('cliente');
+            $table->string('escopo_inicial');
+            $table->string('data_inicio');
+            $table->string('slug');
+            $table->boolean('ativo')->default(1);
+
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->unsignedBigInteger('status_id')->nullable();
+            $table->foreign('status_id')->nullable()->references('id')->on('status')->onUpdate('cascade');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onUpdate('cascade');
+            $table->unsignedBigInteger('updated_by');
+            $table->foreign('updated_by')->references('id')->on('users')->onUpdate('cascade');
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->foreign('deleted_by')->nullable()->references('id')->on('users')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projetos');
+    }
+};
