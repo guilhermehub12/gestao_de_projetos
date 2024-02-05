4c43667bcd49e59b78c9429cb93fd3b3131aaec3
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-02-05 02:42:56 -0300

    criando rotas para criação do projeto

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\ResetPassword;
 use App\Http\Controllers\ChangePassword;
+use App\Http\Controllers\ProjectController;
 
 
 Route::get('/', function () {
@@ -30,19 +31,19 @@ Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('
 Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
-
-    Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
-    
     Route::get('/profile', [UserProfileController::class, 'show'])->name('profile');
+    Route::get('/user-management', [UserProfileController::class, 'show'])->name('user-management');
     Route::post('/profile', [UserProfileController::class, 'update'])->name('profile.update');
 
-    Route::get('/profile-static', [PageController::class, 'profile'])->name('profile-static');
-
-    Route::get('/sign-in-static', [PageController::class, 'signin'])->name('sign-in-static');
-    Route::get('/sign-up-static', [PageController::class, 'signup'])->name('sign-up-static');
-
-    Route::get('/{page}', [PageController::class, 'index'])->name('page');
+    // Route::get('/{page}', [PageController::class, 'index'])->name('page');
 
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 });
